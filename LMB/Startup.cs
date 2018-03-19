05e213bd973919b16d1ef39078a1d9eb67f63046
Javier <Javier@MOVILES2>
2018-03-19 09:16:52 -0500

    Agregar archivos de proyecto.

---
+++ LMB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LMB.Startup))]
+namespace LMB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
